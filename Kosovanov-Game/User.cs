a86637185be9314f97c50f8f567a195f6181b8aa
Вадим <werty193842@gmail.com>
2025-02-18 13:45:36 +0300

    Add files via upload

---
+++ Kosovanov-Game/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public string Name { get; }
+        public Hero Hero { get; }
+
+        public User(string name, Hero hero)
+        {
+            Name = name;
+            Hero = hero;
+
+        }
+    }
+}
